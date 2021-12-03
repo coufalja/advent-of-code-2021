e737741bb64baf53a45d638ef077d70e82584014
Jakub Coufal <jakub.coufal@jamf.com>
2021-12-03 16:57:08 +0100

    Day1

---
+++ Day1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"..\..\..\input.txt"));
+            var count = -1;
+            var last = 0;
+            foreach (var line in lines)
+            {
+                var parsed = int.Parse(line);
+                if (parsed > last)
+                {
+                    count++;
+                }
+                last = parsed;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Day1/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task2
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, @"..\..\..\input.txt"));
+            var windows = new List<int>();
+            for (var i = 0; i < lines.Length; i++)
+            {
+                if (i >= lines.Length - 2)
+                {
+                    break;
+                }
+                windows.Add(0);
+                for (var j = 0; j < 3; j++)
+                {
+                    var parsed = int.Parse(lines[i+j]);
+                    windows[i] += parsed;
+                }
+            }
+
+            var count = -1;
+            var last = 0;
+            foreach (var line in windows)
+            {
+                if (line > last)
+                {
+                    count++;
+                }
+                last = line;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
