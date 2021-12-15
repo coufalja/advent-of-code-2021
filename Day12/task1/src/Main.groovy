2ec114e2e828d2541789437c1954ee53c81691a4
Jakub Coufal <jakub.coufal@jamf.com>
2021-12-15 09:33:31 +0100

    Day 12

---
+++ Day12/task1/src/Main.groovy
@@ -0,0 +1,51 @@
+static def readInput() {
+    def input = new ArrayList<String>()
+    new File("..\\..\\input.txt").withReader('UTF-8') { reader ->
+        String line
+        while ((line = reader.readLine()) != null) {
+            input.add(line)
+        }
+    }
+    return input;
+}
+
+static def parseMap(List<String> input) {
+    def edges = new HashMap<String, List<String>>()
+    for (line in input) {
+        def s = line.split('-')
+        edges.putIfAbsent(s[0], new ArrayList<String>())
+        edges.putIfAbsent(s[1], new ArrayList<String>())
+        edges[s[0]].add(s[1])
+        edges[s[1]].add(s[0])
+    }
+    return edges
+}
+
+static boolean isLowerCase(String s) {
+    for (int i = 0; i < s.length(); i++) {
+        if (!Character.isLowerCase(s.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static def search(Map<String, List<String>> edges, String curr, List<String> prev) {
+    if (isLowerCase(curr)) {
+        prev = prev + [curr]
+    }
+    if (curr == "end") {
+        return 1
+    }
+    def total = 0
+    for (neighbor in edges[curr]) {
+        if (!prev.contains(neighbor)) {
+            total += search(edges, neighbor, prev)
+        }
+    }
+    return total
+}
+
+def input = readInput()
+def map = parseMap(input)
+print("Result: " + search(map, "start", []))
